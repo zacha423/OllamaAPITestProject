a73307bdc2ca58001fb73a3ea9857efc56a4d937
Zachary Abela-Gale <84219877+zacha423@users.noreply.github.com>
2024-05-28 21:16:32 -0700

    Update and rename test.sh to TestAPI.sh

--- TestAPI.sh
+++ TestAPI.sh
@@ -1,3 +1,5 @@
+# Run from the DevContainer's terminal or switch ollama:11434 for localhost:11434.
+#!/bin/bash
 curl ollama:11434/api/pull -d '{"name":"gemma:2b"}'
 curl ollama:11434/api/generate -d '{
   "model":"gemma:2b", 
